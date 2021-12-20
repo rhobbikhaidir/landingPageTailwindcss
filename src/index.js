ab185fdf9ca7d97c8f275c49f31970a7e0c90c4f
rhobbikhaidir <rhobbie09@gmail.com>
2021-12-20 12:20:37 +0700

    fixing styling import in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./config/routes/App";
 import reportWebVitals from "./reportWebVitals";
 // import "./assets/styles/tailwind.css";
-import "./assets/styles/outpu.css";
+import "./assets/styles/output.css";
 
 ReactDOM.render(
   <React.StrictMode>
