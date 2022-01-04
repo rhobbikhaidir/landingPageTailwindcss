583b4a66ca030a0d1c098d0207601a0fd2b330c2
rhobbikhaidir <rhobbie09@gmail.com>
2022-01-04 11:13:54 +0700

    fixing error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./config/routes/App";
-import reportWebVitals from "./reportWebVitals";
-import "./assets/styles/tailwind.css";
-// import "./assets/styles/output.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './config/routes/App';
+import reportWebVitals from './reportWebVitals';
+// import "./assets/styles/tailwind.css";
+import './assets/styles/output.css';
 
-import "index.css";
-import "assets/Fonts/Mulish/Mulish UI.ttf";
-import "assets/Fonts/Mulish/Mulish UI Bold.ttf";
-import "assets/Fonts/Mulish/Mulish UI ExtraLight.ttf";
-import "assets/Fonts/Mulish/Mulish UI SemiBold.ttf";
-import { BrowserRouter } from "react-router-dom";
+import 'index.css';
+import 'assets/Fonts/Mulish/Mulish UI.ttf';
+import 'assets/Fonts/Mulish/Mulish UI Bold.ttf';
+import 'assets/Fonts/Mulish/Mulish UI ExtraLight.ttf';
+import 'assets/Fonts/Mulish/Mulish UI SemiBold.ttf';
+import { BrowserRouter } from 'react-router-dom';
 
 // import "./assets/styles/output.css";
 
@@ -20,7 +20,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
